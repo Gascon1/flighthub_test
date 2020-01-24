fa4e6e132fecc1760d9e6fbab3ebe057b681c4cd
Gascon1 <mikelgascon@outlook.com>
2020-01-23 23:10:08 -0500

    make starship component and test it

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import People from "./components/People";
 import Planets from "./components/Planets";
+import Starships from "./components/Starships";
 
 function App() {
-  return <People />;
+  return <Starships />;
 }
 
 export default App;
